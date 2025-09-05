e5a5503306a186aa66a5c76cebd0f1380d2c2d2d
ZahraaMohd-c <202203702@stu.uob.edu.bh>
2025-09-05 15:50:22 +0300

    post views edit

--- main_app/views.py
+++ main_app/views.py
@@ -25,13 +25,13 @@ class PostListView(ListView):
     model = Post
     template_name = 'post/post_list.html'
     context_object_name = 'posts'
-
+   
     def get_queryset(self):
         queryset = super().get_queryset()
         query = self.request.GET.get('q')
         if query:
             queryset = queryset.filter(post_content__icontains=query)
-        return queryset
+        return queryset.order_by('-post_date')
 
 class PostCreateView(LoginRequiredMixin,CreateView):
     model = Post
@@ -49,7 +49,7 @@ class PostUserView(LoginRequiredMixin,ListView):
     context_object_name = 'posts'
 
     def get_queryset(self):
-        return Post.objects.filter(user=self.request.user)
+        return Post.objects.filter(user=self.request.user).order_by('-post_date')
 
 class PostDetailsView(DetailView):
     model = Post
